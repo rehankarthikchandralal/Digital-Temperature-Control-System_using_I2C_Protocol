d92f57921bfad0eb4ce260128acdbdde4ecbcd5c
rehankarthikchandralal <rehankarthik12@gmail.com>
2025-01-23 00:43:13 +0100

    Update ram.v

--- src/ram.v
+++ src/ram.v
@@ -1,27 +1,21 @@
 module RAM128x32 #(
-    parameter Data_width = 32, // Number of bits in each word
-    Addr_width = 7           // Number of address bits
+    parameter Data_width = 32, 
+    Addr_width = 7          
 )(
     // Ports
     input wire clk,
-    input wire we,           // Write enable
-    input wire [(Addr_width-1):0] address, // Address input
-    input wire [(Data_width-1):0] d,        // Data input
-    output reg [(Data_width-1):0] q        // Registered output
+    input wire we,           
+    input wire [(Addr_width-1):0] address, 
+    input wire [(Data_width-1):0] d,        
+    output reg [(Data_width-1):0] q        
 );
-
-    // Declare a memory array with 128 locations, each 32 bits wide
     reg [Data_width-1:0] ram [0:(2**Addr_width)-1];
 
-    // Always block for synchronous write and read operations
     always @(posedge clk) begin
         if (we) begin
-            // Write operation: store data at the specified address
             ram[address] <= d;
         end
-        // Read operation: register the output on every clock cycle
         q <= ram[address];
     end
-
 endmodule
 
