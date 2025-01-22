2f418b9bd2b36e6e40c6e8d0e1e2feff0322da31
rehankarthikchandralal <rehankarthik12@gmail.com>
2025-01-23 00:40:40 +0100

    Update binary_counter.v

--- src/binary_counter.v
+++ src/binary_counter.v
@@ -1,28 +1,24 @@
 module LS161a (
-    input [3:0] D,       // Parallel Input
-    input CLK,           // Clock
-    input CLR_n,         // Active Low Asynchronous Reset
-    input LOAD_n,        // Enable Parallel Input
-    input ENP,           // Count Enable Parallel
-    input ENT,           // Count Enable Trickle
-    output reg [3:0] Q,  // Parallel Output
-    output RCO           // Ripple Carry Output (Terminal Count)
+    input [3:0] D,       
+    input CLK,           
+    input CLR_n,         
+    input LOAD_n,        
+    input ENP,           
+    input ENT,           
+    output reg [3:0] Q,  
+    output RCO           
 );
 
 always @(posedge CLK or negedge CLR_n) begin
     if (!CLR_n) begin
-        // Active Low Asynchronous Reset
         Q <= 4'b0000;
     end else if (!LOAD_n) begin
-        // Parallel Load
         Q <= D;
     end else if (ENP && ENT) begin
-        // Counting
         Q <= Q + 1;
     end
 end
 
-// Ripple Carry Output (Terminal Count)
 assign RCO = (Q == 4'b1111) && ENT;
 
 endmodule
