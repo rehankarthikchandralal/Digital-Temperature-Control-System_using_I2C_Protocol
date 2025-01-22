f909a0bc48ae8f3bddcff1b943ba94eca44c9a50
rehankarthikchandralal <rehankarthik12@gmail.com>
2025-01-23 00:41:05 +0100

    Update comparator.v

--- src/comparator.v
+++ src/comparator.v
@@ -1,23 +1,4 @@
 `timescale 10ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 27.11.2024 17:03:22
-// Design Name: 
-// Module Name: comparator
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 module comparator #(parameter BIT_WIDTH=4)(
     input wire [BIT_WIDTH-1:0] input_1,
@@ -27,7 +8,6 @@ module comparator #(parameter BIT_WIDTH=4)(
     output wire output_lower
     );
 
-// Use continuous assignments directly for the outputs
 assign output_greater = (input_1 > input_2);
 assign output_equal = (input_1 == input_2);
 assign output_lower = (input_1 < input_2);
